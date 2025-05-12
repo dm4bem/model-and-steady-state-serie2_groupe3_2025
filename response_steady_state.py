8c5f32552b9b99255c87eac426cc974ac9ac39ea
CorentinB73 <corentin.blanchedeau@gmail.com>
2025-05-12 08:57:52 +0200

    Update response_steady_state.py

--- response_steady_state.py
+++ response_steady_state.py
@@ -1,3 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import dm4bem
+from IPython.display import display
+
+from matrice_A.py import *
+from Steady_state.py import*
 # MODEL
 # =====
 # Thermal circuit
